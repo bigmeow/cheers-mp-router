6dbaa73e2ff46b405328bbbff19156127e41041c
lizong <harry.li@71360.com>
2021-04-21 14:50:25 +0800

    cli: 删除不必要的sourcemap

--- rollup.config.ts
+++ rollup.config.ts
@@ -1,6 +1,5 @@
 import resolve from '@rollup/plugin-node-resolve'
 import commonjs from '@rollup/plugin-commonjs'
-import sourceMaps from 'rollup-plugin-sourcemaps'
 import camelCase from 'lodash.camelcase'
 import typescript from 'rollup-plugin-typescript2'
 import json from '@rollup/plugin-json'
@@ -11,9 +10,10 @@ const libraryName = 'index'
 
 export default {
   input: `src/${libraryName}.ts`,
+  sourceMap: false,
   output: [
-    { file: pkg.main, name: camelCase(libraryName), format: 'cjs', sourcemap: true },
-    { file: pkg.module, format: 'es', sourcemap: true },
+    { file: pkg.main, name: camelCase(libraryName), format: 'cjs', sourcemap: false, exports: 'default' },
+    { file: pkg.module, format: 'es', sourcemap: false },
   ],
   // Indicate here external modules you don't wanna include in your bundle (i.e.: 'lodash')
   external: [],
@@ -30,9 +30,6 @@ export default {
     // Allow node_modules resolution, so you can use 'external' to control
     // which external modules to include in the bundle
     // https://github.com/rollup/rollup-plugin-node-resolve#usage
-    resolve(),
-
-    // Resolve source maps to the original source
-    sourceMaps(),
+    resolve()
   ],
 }
